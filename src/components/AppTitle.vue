c6da63aabf333c8c0122db11aa39c8b0b41c44ba
MirkoCherchi <mirkocherchi1992@gmail.com>
2023-12-06 16:36:12 +0100

    feat: insert title

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    name: 'h1',
+    data() {
+        return {
+            title: 'La mia prima app con Vite!',
+        }
+    }
+
+}
+</script>
+
+
+<template>
+    <h1>{{ title }}</h1>
+</template>
+
+<style scoped>
+h1 {
+    color: green;
+}
+</style>
